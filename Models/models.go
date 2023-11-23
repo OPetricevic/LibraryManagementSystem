665104b9656fd71de5e3523dcb28c2661503ac74
Omarep <omar.petricevic@gmail.com>
2023-11-24 00:12:21 +0100

    Implementing Models.

--- Models/models.go
+++ Models/models.go
@@ -1,1 +1,19 @@
 package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uuid.UUID `gorm:"type:uuid;primary_key;"` // Use UUID as primary key
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+
+	Email    string `gorm:"unique"`
+	Password string
+	APIKey   string `gorm:"unique"` // Ensure APIKey is unique
+}
