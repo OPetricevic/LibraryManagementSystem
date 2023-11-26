8b7c92ae31e678c44e4c6a5c11356e2b6ee01a44
Omarep <omar.petricevic@gmail.com>
2023-11-26 11:49:15 +0100

    Adding Handlers

--- Handlers/user.go
+++ Handlers/user.go
@@ -1,1 +1,11 @@
 package handlers
+
+import repository "github.com/OPetricevic/LibraryManagementSystem/Repository"
+
+type UserController struct {
+	Repo *repository.UserRepository
+}
+
+func NewUserController(repo *repository.UserRepository) *UserController {
+	return &UserController{Repo: repo}
+}
